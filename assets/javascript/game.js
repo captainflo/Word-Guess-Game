2a5238d8cd65bacf6781c66cd8b1900f17bc0b24
florian lahitte <lahitte.florian@gmail.com>
2018-12-10 00:15:18 -0500

    put javascript fonction

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,36 +1,52 @@
 
-var alphabet = ["a", "b", "c"];
+var alphabet = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"];
 var win = 0;
 var loose = 0;
 var round = 0;
 
-function turn(){
-    if (round < 9){
-        play();      
-    }
-    else {
-        alert("end of the game");  
-    } 
-}
 
-function play(){
+var welcome = document.getElementById("welcome");
+var userChoiceText = document.getElementById("userchoice-text");
+var computerChoiceText = document.getElementById("computer-text");
+var winText = document.getElementById("winText");
+var looseText = document.getElementById("looseText");
+var looseText = document.getElementById("roundText");
+
+
     document.onkeyup = function(event){
         var userChoice = event.key; 
         var computerChoice = alphabet[Math.floor(Math.random() * alphabet.length)];
-        if(userChoice == computerChoice){
-            win ++;
-            round++;
-            console.log("you win" + win +" you loose " + loose + " the round is " + round);
+        if (round < 9){ 
+            if(userChoice == computerChoice){
+                win ++;
+                round++;
+                console.log("you win" + win +" you loose " + loose + " the round is " + round);
+            }
+            else{
+                loose ++;
+                round++;
+                console.log("you win" + win +" you loose " + loose + " the round is " + round);
+            }   
         }
         else{
-            loose ++;
-            round++;
-            console.log("you win" + win +" you loose " + loose + " the round is " + round);
+            if (win > loose ){
+                alert("end of the game you have win " + win + " times"); 
+            }
+            else{
+                alert("end of the game you have loose " + loose + " times"); 
+            }
         }
-    };
-};
+        welcome.textContent = ""
+        userChoiceText.textContent = "You choose: " + userChoice;
+        computerChoiceText.textContent = "Computer: " + computerChoice;
+        winText.textContent = "Wins: " + win;
+        looseText.textContent = "Wins: " + loose;
+        roundText.textContent = "round: " + round;
 
-turn();
+    };
+
+
+
 
 
 
